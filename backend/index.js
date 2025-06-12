c364dd25ee1f16b9b0b315fda25316376793b846
ankityyyy <159771196+ankityyyy@users.noreply.github.com>
2025-06-12 16:02:17 +0530

    add frontend url

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ async function main() {
 main(); 
 
 app.use(cors({
-  origin: "https://collagefrontend.onrender.com",
+  origin: ["http://localhost:5173/"],
   methods: ["GET", "POST"],
   credentials: true
 }));
